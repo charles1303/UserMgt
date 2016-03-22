518c9de651766858ca3b412d87128d70b8757eaf
Charles_Uye <charles.uye@konga.com>
2016-03-22 23:18:11 +0100

    Change the Parser.java to a Singleton class and seperate the User.php from the Document.php

---
+++ quiz-master/Parser.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+/**
+ * This class is thread safe.
+ */
+public class Parser {
+
+private Parser instance;
+
+private File file;
+
+    private Parser() {
+        //lots of initialization code
+    }
+
+    public static synchronized Parser getInstance() {
+        if(instance == null) {
+            instance = new Parser();
+        }
+        return instance;
+    }
+
+
+  
+  public void setFile(File f) {
+    file = f;
+  }
+  public File getFile() {
+    return file;
+  }
+  public String getContent() throws IOException {
+    FileInputStream i = new FileInputStream(file);
+    String output = "";
+    int data;
+    while ((data = i.read()) > 0) {
+      output += (char) data;
+    }
+    return output;
+  }
+  public String getContentWithoutUnicode() throws IOException {
+    FileInputStream i = new FileInputStream(file);
+    String output = "";
+    int data;
+    while ((data = i.read()) > 0) {
+      if (data < 0x80) {
+        output += (char) data;
+      }
+    }
+    return output;
+  }
+  public void saveContent(String content) throws IOException {
+    FileOutputStream o = new FileOutputStream(file);
+    for (int i = 0; i < content.length(); i += 1) {
+      o.write(content.charAt(i));
+    }
+  }
+}
